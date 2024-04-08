3e2ca8f3d6dc1b7280e971c3761701ebb761d600
iamstace <165945572+iamstace@users.noreply.github.com>
2024-04-08 18:03:07 +0100

    guess count added
    created guess count (beginning at 1) and added it to the output when the correct guess is found

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -3,6 +3,9 @@ import random
 # pick a random number for the user to guess
 rand = random.randint(1, 20)
 
+# initialize a counter for the guesses
+guess_count = 1
+
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
 
@@ -15,4 +18,7 @@ while guess != rand:  # if the guess is not equal to the random number, you have
     print('Enter a new guess: ')
     guess = int(input())
 
-print('You got it! The number was {}'.format(rand))
+    # increment the guess count
+    guess_count += 1
+
+print('You got it! The number was {}. You guessed it in {} tries.'.format(rand, guess_count))
